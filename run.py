e4f364689a945a091eb483804d959fd30e6f25a5
quincyhopper <216158136+quincyhopper@users.noreply.github.com>
2026-02-18 11:25:58 +0000

    Added data split, wrapped script in main guard

--- run.py
+++ run.py
@@ -1,40 +1,20 @@
 import torch
-from data_builder import build_supervised_dataset
+import pandas as pd
+from tqdm import tqdm 
+from data_builder import build_supervised_dataset, filter_authors
 from transformers import AutoTokenizer
 from model import ModelWrapper
 from trainer import ContrastiveTrainer
-import pandas as pd
-from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 MODEL_CODE = 'prajjwal1/bert-tiny'
-DATA_PATH = 'src/data/blogtext.csv'
+DATA_PATH = 'data/blogtext.csv'
 EPOCHS = 20
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-GLOBAL_BATCH_SIZE = 64
+GLOBAL_BATCH_SIZE = 256
 VIEW_SIZE = 3
-MAX_LEN = 512
+MAX_LEN = 32
 MINIBATCH_SIZE = 8
-DOCS_TO_SAMPLE = 512
-
-tokeniser = AutoTokenizer.from_pretrained(MODEL_CODE)
-model = ModelWrapper(MODEL_CODE).to(DEVICE)
-
-train_loader = build_supervised_dataset(
-    pd.read_csv(DATA_PATH),
-    tokeniser=tokeniser,
-    batch_size=GLOBAL_BATCH_SIZE,
-    view_size=VIEW_SIZE,
-    max_seq_len=MAX_LEN,
-    max_docs=DOCS_TO_SAMPLE
-)
-
-train_model = ContrastiveTrainer(model,
-                                 device=DEVICE,
-                                 learning_rate=1e-4,
-                                 weight_decay=None, 
-                                 epochs=EPOCHS, 
-                                 minibatch_size=MINIBATCH_SIZE
-                                 )
 
 def train(trainer, train_loader, epochs):
     trainer.model.train()
@@ -43,13 +23,47 @@ def train(trainer, train_loader, epochs):
 
         epoch_loss = 0.0
 
-        for step, batch in enumerate(train_loader):
+        for batch in tqdm(train_loader):
 
             loss = trainer.train(batch)
             epoch_loss += loss
 
-        print(f"Epoch [{epoch+1}/{epochs}] | Train loss {epoch_loss}")
-
+        print(f"Epoch [{epoch+1}/{epochs}] | Train loss {epoch_loss/len(train_loader)}")
 
 if __name__ == "__main__":
+
+    # Initialise tokeniser and model
+    tokeniser = AutoTokenizer.from_pretrained(MODEL_CODE)
+    model = ModelWrapper(MODEL_CODE).to(DEVICE)
+
+    # Prepare data and loader
+    print(f"Initialising loader")
+    df = pd.read_csv(DATA_PATH)        # 681,284 rows
+    df = filter_authors(df, VIEW_SIZE) # Remove authors that don't have enough texts
+
+    train_df, val_df = train_test_split(
+        df,
+        train_size=0.05,
+        stratify=df['author']
+    )
+
+    train_loader = build_supervised_dataset(
+        train_df,
+        tokeniser=tokeniser,
+        batch_size=GLOBAL_BATCH_SIZE,
+        view_size=VIEW_SIZE,
+        max_seq_len=MAX_LEN
+    )
+
+    # Initialise trainer
+    print(f"Intialising trainer")
+    train_model = ContrastiveTrainer(model,
+                                    device=DEVICE,
+                                    learning_rate=1e-4,
+                                    weight_decay=None, 
+                                    epochs=EPOCHS, 
+                                    minibatch_size=MINIBATCH_SIZE
+                                    )
+    
+    print("Starting training")
     train(train_model, train_loader, EPOCHS)
