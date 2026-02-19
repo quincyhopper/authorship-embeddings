66b5cc9c5fe55a307ed0016a6b443bfeb7149ce2
quincyhopper <216158136+quincyhopper@users.noreply.github.com>
2026-02-19 22:52:44 +0000

    devices 1

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     trainer = L.Trainer(
         max_epochs=MAX_EPOCHS,
         accelerator="gpu",
-        devices=4,
+        devices=1,
         strategy='ddp_find_unused_parameters_true',
         precision="16-mixed",
         callbacks=[checkpoint_callback, early_stopping_callback],
